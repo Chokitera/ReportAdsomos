795abdff7548fc85ab90eb4f26f195e8aff46f11
Alan Alves <100495209+Chokitera@users.noreply.github.com>
2023-09-26 16:54:07 -0300

    Adicionar arquivos de projeto.

---
+++ ReportAdsomosBackEnd/Controllers/FilaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportAdsomosBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilaController : ControllerBase
+    {
+        /* 
+          REQUISITO: COLETAR OS DADOS DA CENTRAL E TRANSFORMAR EM JSON, SOMENTE AS INFORMAÇÕES ABAIXO:
+            - AGENTE
+            - FILA
+            - STATUS
+            - TEMPO EM LIGAÇÃO
+         
+          DEPOIS COM ESSAS INFORMAÇÕES MONTAR UMA ROTA QUE DEVOLVE ESSES DADOS PARA O FRONTEND.
+          NECESSÁRIO CRIAR TAMBÉM UMA OUTRA ROTA PARA RETORNAR OS VALORES EM FORMA DE RELATÓRIO, INICIALMENTE SEM PAGINAÇÃO.
+          SALVAR OS DADOS DO RELATÓRIO NO BANCO, NO CASO O RELATÓRIO VAI SE BASEAR NOS DADOS DO BANCO.
+         
+         */
+        
+    }
+}
