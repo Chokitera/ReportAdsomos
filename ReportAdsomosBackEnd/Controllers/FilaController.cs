768f2c8054b1d0780166346a8c5c30c0e75d9099
Alan <gapapp136@gmail.com>
2024-03-11 14:29:29 -0300

    Inicio da FilaController

--- ReportAdsomosBackEnd/Controllers/FilaController.cs
+++ ReportAdsomosBackEnd/Controllers/FilaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
 
 namespace ReportAdsomosBackEnd.Controllers
 {
@@ -7,6 +8,9 @@ namespace ReportAdsomosBackEnd.Controllers
     [ApiController]
     public class FilaController : ControllerBase
     {
+        private readonly string urlCentral;
+        private readonly string urlRelatorio;
+        private readonly string token;
         /* 
           REQUISITO: COLETAR OS DADOS DA CENTRAL E TRANSFORMAR EM JSON, SOMENTE AS INFORMAÇÕES ABAIXO:
             - AGENTE
@@ -19,6 +23,58 @@ namespace ReportAdsomosBackEnd.Controllers
           SALVAR OS DADOS DO RELATÓRIO NO BANCO, NO CASO O RELATÓRIO VAI SE BASEAR NOS DADOS DO BANCO.
          
          */
-        
+
+        public FilaController()
+        {
+            urlCentral = "http://192.168.0.6/integrador/public/index";
+            urlRelatorio = "";
+            token = "";
+        }
+
+        [HttpGet("fila")]
+        public string GetFila()
+        {
+            try
+            {
+                var Client = this.ServiceHTTPClient;
+                //Consulta
+                var consult = Client.GetAsync("").Result;
+
+                if (consult == null)
+                {
+                    return "Não foi possível consultar a fila!";
+                }
+                return consult.Headers.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        ////public void GeraToken()
+        ////{
+        ////    try
+        ////    {
+
+        ////    }
+        ////    catch (Exception)
+        ////    {
+
+        ////    }
+        ////}
+
+        public HttpClient ServiceHTTPClient
+        {
+            get
+            {
+                var objHTTPClient = new HttpClient();
+                objHTTPClient.Timeout = new TimeSpan(0, 5, 0);
+                objHTTPClient.BaseAddress = new Uri(urlCentral);
+
+                return objHTTPClient;
+            }
+
+        }
     }
 }
